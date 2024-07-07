a146547e08d269b39aa0f1e94eee6cd1d5a97eb3
EgorPlat <egorplatonov2002@gmail.com>
2024-07-07 09:08:07 +0300

    fix-messages-in-dialog

--- dist/guards/fileSize.guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
-import { Observable } from "rxjs";
-export declare class FileSizeGuard implements CanActivate {
-    private readonly maxTotalSize;
-    constructor(maxTotalSize: number);
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>;
-}
--- dist/middlewares/errorLogging.middleware.d.ts
+++
@@ -1,4 +0,0 @@
-import { ConsoleLogger } from '@nestjs/common';
-export declare class MyLogger extends ConsoleLogger {
-    error(message: any, stack?: string, context?: string): void;
-}
